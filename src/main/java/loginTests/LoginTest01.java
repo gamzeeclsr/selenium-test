28f79bb5f2294e6dd0b360f261e15ada1fbeb696
gamzeeclsr <gamzeeclsr@gmail.com>
2025-07-25 16:16:52 +0300

    Yeni login testleri eklendi

---
+++ src/main/java/loginTests/LoginTest01.java
@@ -0,0 +1,26 @@
+package loginTests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest01 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String link = "http://api.guneyeksport.com:82/";
+        driver.get(link);
+
+        WebElement kullanici_adi = driver.findElement(By.id("username"));
+        kullanici_adi.sendKeys("");
+        WebElement devam_et = driver.findElement(By.xpath("//*[@id='app']/div[3]/div/div[2]/div[2]/div/div[2]/button"));
+        devam_et.click();
+
+    }
+
+}
