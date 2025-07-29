dbb12212c373d538e985eed1ca17a9f463911187
gamzeeclsr <gamzeeclsr@gmail.com>
2025-07-29 10:18:04 +0300

    29.07.2025

--- src/main/java/loginTests/LoginTest02.java
+++ src/main/java/loginTests/LoginTest02.java
@@ -7,30 +7,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginTest01 {
+public class LoginTest02 {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         String link = "https://apitest.guneyeksport.com/";
         driver.get(link);
-
         WebElement kullanici_adi = driver.findElement(By.id("username"));
-        kullanici_adi.sendKeys("");
+        kullanici_adi.sendKeys("gamze");
         WebElement devam_et = driver.findElement(By.xpath("//*[@id='app']/div[3]/div/div[2]/div[2]/div/div[2]/button"));
         devam_et.click();
-
-        WebElement uyari_mesaji = driver.findElement(By.id("swal2-html-container"));
-        String mesaj = uyari_mesaji.getText();
-
-        if (mesaj.equals("Lütfen kullanıcı adınızı girin")) {
-            System.out.println("TEST BAŞARILI");
-
-        } else {
-            System.out.println("TEST BAŞARISIZ");
-        }
-
     }
 
 }
